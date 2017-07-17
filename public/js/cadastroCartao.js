0c71fccb27b50cfc2685a50dd6a144017b87ccb6
Note\Diogo <diogo.skopinski@gmail.com>
2017-07-17 10:35:19 -0300

    Focus no campo de erro PagSeguro.

--- public/js/cadastroCartao.js
+++ public/js/cadastroCartao.js
@@ -24,6 +24,7 @@ $(document).ready(function () {
 
 		if($("#txtEmail").val() != $("#txtEmailConfirma").val()){
 			alert('E-mail diferente com o E-mail de confirmação, favor verifique !');
+			$('#txtEmailConfirma').focus();
 			return false;
 		}
 
@@ -31,16 +32,19 @@ $(document).ready(function () {
 
 		if($("#txtSenha").val() != $("#txtSenhaConfirma").val()){
 			alert('Senha diferente com a Senha de confirmação, favor verifique !');
+			$('#txtSenhaConfirma').focus();
 			return false;
 		}
 
 		if($('#txtNomeCompleto').val() == '') { 
 			alert('Necessário preenchar o Nome completo !');
+			$('#txtNomeCompleto').focus();
 			return false;
 		}
 
 		if( $('#sexoM').prop('checked') == false && $('#sexoF').prop('checked') == false ) {
 			alert('Necessário escolher o Sexo!');
+			$('#sexoM').focus();
 			return false;
 		}
 
@@ -51,56 +55,61 @@ $(document).ready(function () {
 
 		if($('#maiorS').prop('checked') == false && $('#maiorN').prop('checked')) { 
 			alert('Necessário marcar se é ou não Maior de 18 anos!');
+			$('#maiorS').focus();
 			return false;
 		}
 
 		if($('#txtCpf').val() == '') { 
 			alert('Necessário preenchar p CPF !');
-			return false;
-		}
-
-		if($('#txtCpf').val() == '') { 
-			alert('Necessário preenchar p CPF !');
+			$('#txtCpf').focus();
 			return false;
 		}
 
 		if($('#nomeCompletoMae').val() == '') { 
 			alert('Necessário preenchar o Nome completo da mãe !');
+			$('#nomeCompletoMae').focus();
 			return false;
 		}
 
 		if($('#txtTelResidencial').val() == '' && $('#txtTelCelular').val() ) { 
 			alert('Necessário preencher algum telefone Residencial ou Celular!');
+			$('#txtTelCelular').focus();
 			return false;
 		}
 
 		if($('#txtCep').val() == '') { 
 			alert('Necessário preenchar o CEP !');
+			$('#txtCep').focus();
 			return false;
 		}
 
 		if($('#txtEndereco').val() == '') { 
 			alert('Necessário preenchar o Endereço !');
+			$('#txtEndereco').focus();
 			return false;
 		}
 
 		if($('#numNumero').val() == '') { 
 			alert('Necessário preenchar o Número !');
+			$('#numNumero').focus();
 			return false;
 		}
 
 		if($('#txtEstado').val() == '') { 
 			alert('Necessário preenchar o Estado !');
+			$('#txtEstado').focus();
 			return false;
 		}
 
 		if($('#txtCidade').val() == '') { 
 			alert('Necessário preenchar a Cidade !');
+			$('#txtCidade').focus();
 			return false;
 		}
 
 		if($('#txtBairro').val() == '') { 
 			alert('Necessário preenchar o Bairro !');
+			$('#txtBairro').focus();
 			return false;
 		}
 
