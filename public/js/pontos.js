d9002ab444159a78afe4203ba1502491a69ffe01
Note\Diogo <diogo.skopinski@gmail.com>
2017-07-29 11:02:42 -0300

    Ajuste ao clicar no cartão já faz validação do usuário logado, ajuste nos pontos da home.

--- public/js/pontos.js
+++ public/js/pontos.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     anjo.commonEvents();
 });
+
+function verificaLoginCartao(){
+	firebase.auth().onAuthStateChanged(function(user) {
+	  	if (user) {
+			window.location.href = 'cadastroCartaoPagSeguro.html';
+	  	} else {
+	    	alert('É necessário o usuário logar no site!');
+	    	return false;
+	  	}
+	});
+}
